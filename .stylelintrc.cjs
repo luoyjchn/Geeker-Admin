38bb5cd19e27630b97715b8ed9a9a7509feebda9
luoyjchn <luoyjchn@gmail.com>
2025-02-27 09:44:00 +0800

    build: 📦️ 添加 stylelint 规则

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -34,6 +34,14 @@ module.exports = {
       {
         ignorePseudoClasses: ["global", "v-deep", "deep"]
       }
+    ],
+    "declaration-property-value-no-unknown": [
+      true,
+      {
+        ignoreProperties: {
+          "/.+/": "/v-bind\(.+\)/"
+        }
+      }
     ]
   },
   ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"]
